66c24545d5f6892595abb38c417702c664cb4595
Ralph Lee <dev.leeralph@gmail.com>
2024-10-20 19:59:25 +0800

    Added demo files.

---
+++ index.js
@@ -0,0 +1,27 @@
+/** 01. Simple Node.js application */
+// console.log('Hello, Ralph Lee!');
+
+/** 02. Console Module */
+// const petsArray = ['dog', 'cat', 'bird', 'monkey'];
+
+// console.log(petsArray);
+// console.table(petsArray);
+// console.assert(petsArray.length > 5);
+
+/** 03. Process Module. NodeJS application with Process Arguments */
+/** node index.js Juan 'Dela Cruz' */
+// console.log(`First Name: ${process.argv[2]} \nLast Name: ${process.argv[3]}`);
+
+/** 04. OS Module */
+// import os from 'os';
+// const server = {
+//     type: os.type(),
+//     architecture: os.arch(),
+//     uptime: os.uptime()
+// };
+// console.log(server);
+
+/** 05. Using NPM  */
+/** npm install chalk */
+// import chalk from 'chalk';
+// console.log(chalk.bgCyan('Hello world!'));
